d167ce0712ce4cbb8762ec15d3d81fc03f8ca3ed
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-03-24 16:09:17 -0400

    Final 24th

--- src/main.c
+++ src/main.c
@@ -6,23 +6,22 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 04:31:19 by jbernard          #+#    #+#             */
-/*   Updated: 2023/03/24 15:15:25 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/03/24 15:59:34 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 #include <termios.h>
 
-int is_working;
-
 void ctrlc_handle(){
-	ft_putchar_fd('\n', 1);
-	rl_on_new_line();
-	rl_replace_line("", 0);
-	rl_redisplay();
+	ft_putchar_fd('\n', 1); 							// Jump on a clean line
+	rl_on_new_line();									// Tell readline we are on a new line
+	rl_replace_line("", 0);								// Replace readline buffer with empty
+	rl_redisplay();										// Rewrite prompt with empty str
 }
 
-void ctrlslash_handle(int sig){
+void sigquit_handle(int sig){
+	//It's not because a function does nothing that it's useless
 	(void)sig;
 }
 
@@ -31,10 +30,10 @@ void	prompt_loop(void){
 	char *delim = " ";
 
 	while (1){
-		char* input = readline("minishell> ");
+		char* input = readline("minishell$ ");
 		if (input == NULL)
 			exit(0);
-		add_history(input);
+		add_history(input);								// Add current input to the history list
 		token = ft_strtok(input, delim);
 		printf(" -> %s\n", token);
 		while (token != NULL){
@@ -49,26 +48,26 @@ void set_new_termios(struct termios old_termios){
 	struct termios new_termios;
 	
 	new_termios = old_termios;
-	new_termios.c_cc[VQUIT] = _POSIX_VDISABLE;
-	new_termios.c_lflag &= ~ECHOCTL;
-	tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+	new_termios.c_cc[VQUIT] = _POSIX_VDISABLE;			// Disable printing of ^\ when doing ctrl-\ --
+	new_termios.c_lflag &= ~ECHOCTL;					// Disable printing of ^c when doing ctrl-c
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);		// Set new settings to current terminal
 }
 
 int main(int argc, char **argv, char **envp) {
 	struct termios old_termios;
 	
-	tcgetattr(STDIN_FILENO, &old_termios);
+	tcgetattr(STDIN_FILENO, &old_termios);				// Get parent terminal settings
 	set_new_termios(old_termios);
 	
 	signal(SIGINT, ctrlc_handle);
-	signal(SIGQUIT, ctrlslash_handle);
+	signal(SIGQUIT, sigquit_handle);
 	
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	
 	prompt_loop();
-	tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+	tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);		// Go back to old terminal settings.
 	
 	return 0;
 }
