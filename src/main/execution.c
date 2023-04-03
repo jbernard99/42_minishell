3733e7cbd01bbd97bc88268e4ad57a69a84946c5
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-04-03 14:09:30 -0400

    Updating tools

--- src/main/execution.c
+++ src/main/execution.c
@@ -6,7 +6,7 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 14:26:47 by jbernard          #+#    #+#             */
-/*   Updated: 2023/04/03 14:03:08 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/04/03 14:06:49 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,48 +23,7 @@
 
 char *builtins[7] = {"cd", "notecho", "env", "exit", "export", "pwd", "unset"};
 
-char	*ft_strpbrk(const char *str, const char *delim, int *flags)
-{
-	const char *ptr1;
-	const char *ptr2;
-	
-	ptr1 = str;
-	while (*ptr1 != '\0')
-	{
-		ptr2 = delim;
-		while (*ptr2 != '\0')
-		{
-			if (*ptr1 == '\'' || *ptr1 =='\"')
-			{
-				if (*ptr1 == '\'')
-					*flags ^= QUOTE;
-				else if(*ptr1 == '\"')
-					*flags ^= DQUOTE;
-			}
-			if (*ptr1 == *ptr2 && (*flags & (QUOTE | DQUOTE)) == 0)
-				return ((char*)ptr1);
-			ptr2++;
-		}
-		ptr1++;
-	}
-	return (NULL);
-}
 
-char	*ft_strtok(char *str, const char *delim, int *flags)
-{
-	static char	*last_token;
-	char		*token;
-
-	if (str != NULL)
-		last_token = str;
-	else if(last_token == NULL)
-		return (NULL);	
-	token = last_token;
-	last_token = ft_strpbrk(last_token, delim, flags);
-	if (last_token)
-		*last_token++ = '\0';
-	return (token);
-}
 
 void print_str_lst(char **lst)
 {
