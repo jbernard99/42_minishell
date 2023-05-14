df63798341392d49f704db8f0e2aee1e6a0bbbf2
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-05-14 16:33:28 -0400

    Working on pipin

--- src/main/pipe.c
+++ src/main/pipe.c
@@ -6,17 +6,27 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 14:37:01 by jbernard          #+#    #+#             */
-/*   Updated: 2023/05/10 18:42:42 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/05/14 14:41:02 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int		pipex(int id)
+int		pipex(pid_t pid)
 {
 	int		fd[2];
-	pid_t 	pid;
-	(void)id;
-	(void)pid;
+	if (pipe(fd) == -1) {
+		perror("pipe");
+	}
+	if (pid == 0) {
+		close(fd[1]);
+		if (dup2(fd[0], STDIN_FILENO) == -1)
+			perror("dup2");
+	}
+	else {
+		close(fd[0]);
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			perror("dup2");
+	}
 	return fd[0];
 }
