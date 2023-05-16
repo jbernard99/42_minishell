0f0872f87b28441984af20c0b728645ad1706d64
Mathieu Gagnon <mgagnon@c2r2p8.42quebec.com>
2023-05-16 15:15:07 -0400

    update redirect

--- src/redirect/redirect_in_tools.c
+++ src/redirect/redirect_in_tools.c
@@ -1,55 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redirect_tools.c                                   :+:      :+:    :+:   */
+/*   redirect_in_tools.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgagnon <mgagnon@student.42quebec.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/11 10:09:47 by mgagnon           #+#    #+#             */
-/*   Updated: 2023/05/15 15:18:36 by mgagnon          ###   ########.fr       */
+/*   Created: 2023/05/16 14:52:21 by mgagnon           #+#    #+#             */
+/*   Updated: 2023/05/16 15:14:10 by mgagnon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	append(char *input, char *file)
+int	reset_stdin(int old_fd)
 {
-	int	input_len;
-	int	fd;
-	size_t	wr_bytes;
-
-	input_len = ft_strlen(input);
-	fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	if (fd == -1)
+	if (dup2(old_fd, STDIN_FILENO) == -1)
 	{
-		perror("open");
-		return (0);
-	}
-	wr_bytes = write(fd, input, input_len);
-	close(fd);
-	if (wr_bytes == -1)
-	{
-		perror("write");
+		perror("reset STDIN");
 		return (0);
 	}
 	return (1);
 }
 
-void	here_doc(const char *delim)
+int	change_stdin(int new_fd)
+{
+	if (dup2(new_fd, STDIN_FILENO) == -1)
+	{
+		perror("dup2 change STDIN");
+		return (0);
+	}
+	return (1);
+}
+
+int	here_doc(int input_fd, const char *delim)
 {
 	char	*input;
 	int	pipefd[2];
 	pid_t	pid;
 
-	/* write(1, "\n", 1); */
 	while (ft_strcmp(input, delim) != 0)
 	{
 		input = readline("> ");
 		if (input == NULL)
 		{
 			free(input);
-			return ;
+			return (0);
 		}
-
+		write(input_fd, input, sizeof(input));
 	}
+	return (1);
 }
