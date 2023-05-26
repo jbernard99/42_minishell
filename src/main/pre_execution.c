97d0818a5ced10daf3ebb025ee51a5cfff6fa78e
Mathieu Gagnon <mgagnon@reception.42quebec.com>
2023-05-26 10:51:23 -0400

    fixed replace env var

--- src/main/pre_execution.c
+++ src/main/pre_execution.c
@@ -6,7 +6,7 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:59:42 by jbernard          #+#    #+#             */
-/*   Updated: 2023/05/25 10:56:46 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/05/26 10:29:34 by mgagnon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ void	work_env_vars_calls(t_cmdlst *cmdlst)
 	while (cmdlst)
 	{
 		i = 0;
-		while (cmdlst->token[i])
+		while (cmdlst->token[i] && cmdlst->token[i][0] != '\'')
 		{
-			if (is_there_env_var(cmdlst->token[i]))
-				rplc_env_var(cmdlst->envlst, cmdlst->token[i]);
+			if (is_there_env_var(cmdlst->token[i]) != 0)
+				cmdlst->token[i] = rplc_env_var(cmdlst->envlst, cmdlst->token[i]);
 			i++;
 		}
 		cmdlst = cmdlst->next;
