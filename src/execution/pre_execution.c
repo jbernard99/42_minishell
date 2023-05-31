2157e8fe7aaa218b4ea18dec748013cfab1b7d08
Julien Bernard <jbernard@c2r4p6.42quebec.com>
2023-05-31 10:59:36 -0400

    ok

--- src/execution/pre_execution.c
+++ src/execution/pre_execution.c
@@ -6,7 +6,7 @@
 /*   By: jbernard <jbernard@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 14:59:42 by jbernard          #+#    #+#             */
-/*   Updated: 2023/05/30 12:39:30 by jbernard         ###   ########.fr       */
+/*   Updated: 2023/05/31 10:46:12 by jbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,30 @@ void	work_env_vars_calls(t_cmdlst *cmdlst)
 		cmdlst = cmdlst->next;
 	}
 }
-
 void	remove_redirection_from_tokens(char **token)
 {
 	char	**n_token;
+	size_t	i;
+	int		j;
+	int		next_is_file;	
 
-	
+	next_is_file = 0;
+	i = ft_strtablen(token);
+	n_token = ft_calloc(i - 2, sizeof(char*));
+	j = 0;
+	while (token[i])
+	{
+		if (ft_strcmp(token[i], "<") == 0 || ft_strcmp(token[i], ">") == 0)
+			i++;
+		else if (ft_strcmp(token[i], "<<") == 0 || ft_strcmp(token[i], ">>") == 0)
+			i++;
+		else
+		{
+			n_token[j] = token[i];
+			j++;
+		}
+		i++;
+	}
 }
 
 char	*get_file(t_cmdlst *cmdlst)
@@ -51,7 +69,10 @@ char	*get_file(t_cmdlst *cmdlst)
 	while (cmdlst->token[i])
 	{
 		if (cmdlst->flags & R_IN && ft_strcmp("<", cmdlst->token[i]))
+		{
+			
 			return (cmdlst->token[i + 1]);
+		}
 		else if (cmdlst->flags & HR_DOC && ft_strcmp("<<", cmdlst->token[i]))
 			return (cmdlst->token[i + 1]);
 		else if (cmdlst->flags & R_OUT && ft_strcmp(">", cmdlst->token[i]))
@@ -68,5 +89,5 @@ void	work_redirection(t_cmdlst *cmdlst)
 	
 	pipe(fds);
 	if (cmdlst->flags & R_IN)
-		redirect_in(fds[1], get_file(cmdlst));
+		redirect_in(fds[1], );
 }
